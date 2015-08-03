27cdd2a6c68f490b4636f0b87d2ab8dc8547f1f5
Steffen Kühne <stekhn@gmail.com>
2015-08-03 16:31:42 +0200

    Add linup display

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,32 @@
-var aufstellung = document.getElementById('aufstellung');
+function getURLParameter(name) {
+
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+        results = regex.exec(location.search);
+
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
 
 function convertLineup(str) {
-	var arr = str.split('x');
 
-	for (var i = 0;  i < arr.length; i++) {
-		arr[i] = arr[i].match(/.{1,2}/g);
-	}
+    var arr = str.split('x');
 
-	console.log(arr);
+    for (var i = 0;  i < arr.length; i++) {
+        arr[i] = arr[i].match(/.{1,2}/g);
+    }
+
+    return arr;
 }
 
-function getURLParameter(name) {
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
-    results = regex.exec(location.search);
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+function showLineup(arr) {
+
+    var lineupEl = document.getElementById('lineup');
+
+    for (var i = 0;  i < arr.length; i++) {
+
+        lineupEl.innerHTML += '<p>' + arr[i].join('\t') + '</p>';
+    }
 }
 
-aufstellung.innerHTML = convertLineup(getURLParameter('aufstellung'));
+showLineup(convertLineup(getURLParameter('aufstellung')));
